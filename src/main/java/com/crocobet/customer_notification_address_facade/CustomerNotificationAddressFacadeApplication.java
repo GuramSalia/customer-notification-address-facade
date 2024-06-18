a03813f08ab984307327d3a9b8e1f0690d1fde10
Guram Salia <guramsalia@gmail.com>
2024-06-18 09:58:20 +0400

    initial commit

---
+++ src/main/java/com/crocobet/customer_notification_address_facade/CustomerNotificationAddressFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.crocobet.customer_notification_address_facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerNotificationAddressFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerNotificationAddressFacadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crocobet/customer_notification_address_facade/CustomerNotificationAddressFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crocobet.customer_notification_address_facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerNotificationAddressFacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
